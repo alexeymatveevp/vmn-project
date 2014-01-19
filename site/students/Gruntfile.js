327cf45eb3045e604da5cd7a00374b45724ea767
david <david.lucky.star@gmail.com>
2014-01-19 04:02:52 +0400

    grunt and bower

---
+++ site/students/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! UGLIFIED! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        qunit: {
+            files: ['static/js/test.html']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.registerTask('default', ['qunit']);
+    grunt.registerTask('pevert', ['concat', 'uglify']);
+};
---
+++ site/students/static/js/script.js
@@ -0,0 +1,52 @@
+$(function() {
+    $.ajax({
+        url: "skills",
+    }).done(function(data) {
+        var skills = eval(data)
+        for (i in skills) {
+            var skill = skills[i]
+            var p = $('<p></p>')
+            p.addClass('block')
+            p.css('position', 'absolute')
+            p.attr('id', skill.name)
+            p.addClass('draggable')
+            p.html(skill.name)
+            $('#skill_tree').append(p)
+
+            for (j in skill.parent) {
+                var parent = skill.parent[j]
+                var con = $('<div class="connector"></div>')
+                var img = $('<img src="/static/img/arrow.gif" class="connector-end"></img>')
+                // var label = $('<label class="source-label">start</label>')
+                con.append(img)
+                // con.append(label)
+                con.addClass(parent)
+                con.addClass(skill.name)
+                con.addClass("down_end")
+                $('#skill_tree').append(con)
+            }
+
+        }
+        makeTree(skills)
+        renderTree()
+        initPageObjects()
+    });
+});
+
+function renderTree() {
+    $('#skill_tree p.block').each(function() {
+        $(this).css('top', 10 + getLevel(this.id) * 50)
+    })
+
+}
+
+function getLevel(nodeName) {
+    var tree = window.treeMap
+    for (i in tree) {
+        for (j in tree[i]) {
+            if (tree[i][j] == nodeName) {
+                return i
+            }
+        }
+    }
+}
